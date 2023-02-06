3ca3605128dc3d00dbbc3296a1bbf0f0f675cbd3
Justin Huth <Jayhuth96@gmail.com>
2023-02-06 13:35:26 -0600

    completed the excersise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -2,9 +2,12 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //infered typing, string interpolation, and the ternary operator.
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+
+            Console.WriteLine(response);
         }
     }
 }
